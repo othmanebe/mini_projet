04651e43241c99923b1bfbb766c836a2e5eb6805
othmanebe <othmane.bellous@gmail.com>
2021-07-11 16:06:34 +0100

    uncomment local database

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,14 @@
 <?php 
 // local
-// $servername= "localhost";
-// $username = "root";
-// $password= "";
+$servername= "localhost";
+$username = "root";
+$password= "";
+$db = "mini_projet_db";
 
-//remote
-$servername= "remotemysql.com";
-$username = "z7VJZFQ4jD";
-$password= "9cbkJFij5c";
-$db ="z7VJZFQ4jD";
+// //remote
+// $servername= "remotemysql.com";
+// $username = "z7VJZFQ4jD";
+// $password= "9cbkJFij5c";
+// $db ="z7VJZFQ4jD";
 $connection = mysqli_connect($servername,$username,$password,$db);
 ?>
